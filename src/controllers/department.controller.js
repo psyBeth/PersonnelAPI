27b6e9b29a9de7ce13825ac7f34ef151840a7715
Betul <betulkoru06@gmail.com>
2024-03-18 22:33:14 +0300

    department controller CRUD completed

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -20,16 +20,30 @@ module.exports = {
         });
     },
     read: async (req, res) => {
-        const data = await Dpeartment.findOne({_id: req.params.id});
+        const data = await Department.findOne({_id: req.params.id});
         res.status(200).send({
             error: false,
             data,
         })
     },
     update: async (req, res) => {
-
+        const data = await Department.updateOne({_id: req.params.id}, req.body, { runValidators: true });
+        res.status(202).send({
+            error: false,
+            data,
+            new: await Department.findOne({_id: req.params.id}),
+        });
     },
     delete: async (req, res) => {
-
+        const data = await Department.deleteOne({_id: req.params.id});
+        // const isDeleted = data.deletedCount >= 1 ? true : false;
+        // res.status(isDeleted ? 204 : 404).send({
+        //     error: !isDeleted,
+        //     data,
+        // });
+        res.status(data.deletedCount ? 204 : 404).send({
+            error: !data.deletedCount,
+            data,
+        });
     },
 }
