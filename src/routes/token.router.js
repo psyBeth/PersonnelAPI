82d3150e88a27d73b2dfe87b8899993fdaf6773e
Betul <betulkoru06@gmail.com>
2024-03-21 21:58:29 +0300

    token router edited for the shorter

--- src/routes/token.router.js
+++ src/routes/token.router.js
@@ -3,18 +3,21 @@
 const router = require('express').Router();
 
 const token = require('../controllers/token.controller');
-const permissions = require('../middlewares/permissions');
+// const permissions = require('../middlewares/permissions');
+const { isAdmin } = require("../middlewares/permissions");
 
 // URL: /tokens
 
+router.use(isAdmin);
+
 router.route('/')
-    .get(permissions.isAdmin, token.list)
-    .post(permissions.isAdmin, token.create)
+    .get(token.list)
+    .post(token.create)
 
 router.route('/:id')
-    .get(permissions.isAdmin, token.read)
-    .put(permissions.isAdmin, token.update)
-    .patch(permissions.isAdmin, token.update)
-    .delete(permissions.isAdmin, token.delete)
+    .get(token.read)
+    .put(token.update)
+    .patch(token.update)
+    .delete(token.delete)
 
 module.exports = router
