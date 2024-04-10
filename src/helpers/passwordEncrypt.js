28739a345dc216315360273cd846bcb9687ad400
Betul <betulkoru06@gmail.com>
2024-04-10 22:05:27 +0300

    password encrypt fixed i guess

--- src/helpers/passwordEncrypt.js
+++ src/helpers/passwordEncrypt.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const crypto = require('node:crypto'),
-    keyCode = process.env.SECRET_KEY,
-    loopCount = 10_000,
-    charCount = 32,
-    encType = 'sha512';
+const crypto = require('node:crypto');
+const keyCode = process.env.SECRET_KEY || 'pls_write_random_chars_here';
+const loopCount = 10_000;
+const charCount = 32;   //32 for 64
+const encType = 'sha512';
 
 module.exports = function (password) {
     return crypto.pbkdf2Sync(password, keyCode, loopCount, charCount, encType)
